091a40ccf8aa190266a28e0fd5c773d9f5ead335
Josh Aragon <josharagon99@gmail.com>
2021-05-27 20:40:58 -0600

    Add Auth0 login and logout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,17 @@
 import './App.css';
-
+import LoginButton from '../LoginButton.js'
+import LogoutButton from '../LogoutButton.js'
 
 
 function App() {
 
-  function showSettings(event) {
-    event.preventDefault();
-
-  }
 
   return (
     <main className='main'>
       <nav>
         <h1>car Meetz</h1>
+        <LoginButton />
+        <LogoutButton />
       </nav>
     </main>
   );
