4735de334348ac78cf543133de94c419132c9570
josharagon <69175998+josharagon@users.noreply.github.com>
2021-05-27 21:40:28 -0600

    Authentication (#1)
    * add hamburger menu
    
    * Remove unworking hamburger
    
    * Add home bar
    
    * Add Auth0 login and logout
    
    * Show user specific details
    
    * Add is loading when loading

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginButton from '../LoginButton.js';
+import LogoutButton from '../LogoutButton.js';
+import Profile from '../Profile.js';
+import { useAuth0 } from '@auth0/auth0-react';
+
 
 function App() {
+const { isLoading } = useAuth0();
+
+
+if(isLoading) return <div>Loading...</div>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='main'>
+      <nav>
+        <h1>car Meetz</h1>
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+      </nav>
+    </main>
   );
 }
 
