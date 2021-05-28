9af981f89685d070d206b6bc79406aa8da5d2b37
Josh Aragon <josharagon99@gmail.com>
2021-05-27 20:02:58 -0600

    Add home bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { slide as Menu } from 'react-burger-menu'
 
 
 
@@ -13,12 +12,6 @@ function App() {
   return (
     <main className='main'>
       <nav>
-        <Menu>
-          <a id="home" className="menu-item" href="/">Home</a>
-          <a id="about" className="menu-item" href="/about">About</a>
-          <a id="contact" className="menu-item" href="/contact">Contact</a>
-          <a onClick={showSettings} className="menu-item--small" href="">Settings</a>
-        </Menu>
         <h1>car Meetz</h1>
       </nav>
     </main>
