8ae02846f44ad0e8894e2adee1c771e07d212fb7
Josh Aragon <josharagon99@gmail.com>
2021-05-27 21:04:54 -0600

    Add is loading when loading

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import LoginButton from '../LoginButton.js'
-import LogoutButton from '../LogoutButton.js'
-import Profile from '../Profile.js'
+import LoginButton from '../LoginButton.js';
+import LogoutButton from '../LogoutButton.js';
+import Profile from '../Profile.js';
+import { useAuth0 } from '@auth0/auth0-react';
 
 
 function App() {
+const { isLoading } = useAuth0();
 
 
+if(isLoading) return <div>Loading...</div>
   return (
     <main className='main'>
       <nav>
